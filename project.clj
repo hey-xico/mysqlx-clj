540569baab0a4c17f25b930605f2e16bf2ea544a
Chico Almeida <francisco.almeida@outlook.com>
2019-10-16 00:01:52 -0300

    added wrapper for session actions

--- project.clj
+++ project.clj
@@ -1,14 +1,29 @@
-(defproject mysqlx-clj "0.0.0"
+(defproject mysqlx-clj "0.0.1"
   :description "FIXME: write description"
   :url "https://github.com/Chicoalmeida/mysqlx-clj"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies []
+  :dependencies [[org.clojure/data.json "0.2.6"]
+                 [mysql/mysql-connector-java "8.0.17"]]
   :plugins [[lein-cloverage "1.0.13"]
             [lein-shell "0.5.0"]
             [lein-ancient "0.6.15"]
             [lein-changelog "0.3.2"]]
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.0"]]}}
+  :profiles {:dev {:resource-paths ["test/resources"]
+                   :plugins        [[com.jakemccrary/lein-test-refresh "0.24.1"]
+                                    [lein-cljfmt "0.6.4"]
+                                    [lein-cloverage "1.1.1"]
+                                    [test2junit "1.2.7"]
+                                    [jonase/eastwood "0.3.5"]
+                                    [lein-kibit "0.1.7"]]
+                   :dependencies   [[org.clojure/clojure "1.10.0"]
+                                    [circleci/bond "0.4.0"]
+                                    ; logging
+                                    [ch.qos.logback/logback-classic "1.1.8" :exclusions [org.slf4j/slf4j-api]]
+                                    [org.slf4j/jul-to-slf4j "1.7.22"]
+                                    [org.slf4j/jcl-over-slf4j "1.7.22"]
+                                    [org.slf4j/log4j-over-slf4j "1.7.22"]
+                                    [org.testcontainers/testcontainers "1.12.2"]]}}
   :deploy-repositories [["releases" :clojars]]
   :aliases {"update-readme-version" ["shell" "sed" "-i" "s/\\\\[mysqlx-clj \"[0-9.]*\"\\\\]/[mysqlx-clj \"${:version}\"]/" "README.md"]}
   :release-tasks [["shell" "git" "diff" "--exit-code"]
--- src/mysqlx_clj/core.clj
+++ src/mysqlx_clj/core.clj
@@ -1,6 +1,101 @@
-(ns mysqlx-clj.core)
+(ns mysqlx-clj.core
+  (:require [clojure.spec.alpha :as s]
+            [clojure.walk :refer [stringify-keys postwalk]])
+  (:import (com.mysql.cj.xdevapi SessionFactory Session Schema)
+           (java.util Properties)))
 
+(s/def ::host string?)
+(s/def ::port number?)
+(s/def ::user string?)
+(s/def ::password string?)
+(s/def ::dbname string?)
+(s/def ::connection-props (s/keys :req-un [::host ::port ::user ::password]
+                                  :opt-un [::dbname]))
 
-(defn foo "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn create-props
+  ^Properties
+  [props]
+  (let [props# (Properties.)
+        stringified-keys (stringify-keys props)
+        f (fn [[k v]] (if (string? v) [k v] [k (.toString v)]))
+        _ (.putAll props# (postwalk (fn [x] (if (map? x) (into {} (map f x)) x)) stringified-keys))]
+    props#))
+
+(defn open-session
+  "Connects to MySql using x protocol."
+  {:arglists '([{:keys [host port user password dbname]}])}
+  [props]
+  {:pre [(s/valid? ::connection-props props)]}
+  (let [session-factory (SessionFactory.)]
+    (.getSession session-factory (create-props props))))
+
+(defn open?
+  [session]
+  (.isOpen session))
+
+(defn get-schema-names
+  "Gets a list of all schema names present on the server"
+  [^Session session]
+  (into [] (map #(.getName %) (.getSchemas session))))
+
+(defn ^Schema get-schema
+  "Get schema reference by name."
+  [^Session session ^String name]
+  (.getSchema session name))
+
+(defn drop-schema
+  "Drops a schema"
+  [^Session session ^String schema]
+  (let [schemas (get-schema-names session)]
+    (if (some #(= schema %)  schemas)
+      (do (println "Calling drop")
+          (.dropSchema session schema))
+      (println "Not Calling drop"))))
+
+(defn create-schemagcb
+  "Creates a schema"
+  [^Session session ^String schema-name]
+  (try
+    (.createSchema session schema-name)
+    (catch Exception e
+      (throw (ex-info (str "Unable to create schema: " (.getMessage e))
+                      {:cause   (.getCause e)
+                       :message (.getMessage e)}))))
+  )
+
+(defn get-url
+  "Get the URL used to create this session."
+  [^Session session]
+  (.getUri session))
+
+(defn close-session
+  "Closes the given x protocol session"
+  [^Session session]
+  (.close session))
+
+(defn start-transaction
+  [^Session session]
+  (.startTransaction session))
+
+(defn commit
+  [^Session session]
+  (.commit session))
+
+(defn rollback
+  [^Session session]
+  (.rollback session))
+
+(defn transactional-execution
+  [^Session session f]
+  (try
+    (start-transaction session)
+    (let [r (f)]
+      (commit session)
+      r)
+    (catch Exception e
+      (do
+        (println "ERRO: " (.getMessage e))
+        (rollback session)
+        )
+      ))
+  )
