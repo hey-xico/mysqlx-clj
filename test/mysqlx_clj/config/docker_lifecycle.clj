6058da8acad1ab4b00a97bc787605db8daa40bae
Chico Almeida <francisco.almeida@outlook.com>
2019-10-16 00:01:52 -0300

    added wrapper for session actions

---
+++ test/mysqlx_clj/config/docker_lifecycle.clj
@@ -0,0 +1,44 @@
+(ns mysqlx_clj.config.docker-lifecycle
+  (:import (org.testcontainers.containers GenericContainer)
+           (org.testcontainers.containers.wait.strategy Wait)))
+
+
+(def connection-properties
+  {"MYSQL_DATABASE"      "mysqlx-clj"
+   "MYSQL_USER"          "clj"
+   "MYSQL_PASSWORD"      "s3cr3t"
+   "MYSQL_ROOT_PASSWORD" "root"
+   "TZ"                  "UTC"})
+
+(defn- container []
+  (let [image-tag "mysql:8.0.13"
+        x-port (into-array Integer [(Integer. 33060)])
+        startup-signal (Wait/forLogMessage ".*X Plugin ready for connections.*\\s" 2)
+        environment connection-properties]
+    (-> (GenericContainer. image-tag)
+        (.withEnv environment)
+        (.withExposedPorts x-port)
+        (.waitingFor startup-signal))))
+
+(defn initialize []
+  (let [mysql-8-container (container)]
+    (.start mysql-8-container)
+    mysql-8-container))
+
+(defn running? [container]
+  (if container
+    (.isRunning container)))
+
+(defn host-address [container]
+  (if container
+    (.getIpAddress container)
+    "localhost"))
+
+(defn host-port [container]
+  (if container
+    (.getMappedPort container 33060)
+    30000))
+
+(defn destroy [container]
+  (.stop container)
+  )
--- test/mysqlx_clj/core_test.clj
+++ test/mysqlx_clj/core_test.clj
@@ -1,8 +1,134 @@
 (ns mysqlx-clj.core-test
   (:require [clojure.test :refer :all]
-            [mysqlx-clj.core :refer :all]))
+            [bond.james :as bond :refer [with-spy]]
+            [mysqlx-clj.core :as target]
+            [mysqlx_clj.config.docker-lifecycle :as docker])
+  (:import (clojure.lang ExceptionInfo)
+           (java.util UUID)))
+
+(def container (atom nil))
+(defn random-string []
+  (first
+    (shuffle
+      (clojure.string/split
+        (.toString (UUID/randomUUID)) #"-"))))
+
+;(use-fixtures :once (fn [f]
+;                      (reset! container (docker/initialize))
+;                      (f)
+;                      (docker/destroy @container)
+;                      ))
+(deftest connection-validations
+  (testing "given valid credentials should start a valid session"
+    (let [;given
+          connection-props {:host     (docker/host-address @container)
+                            :port     (docker/host-port @container)
+                            :dbname   (docker/connection-properties "MYSQL_DATABASE")
+                            :user     (docker/connection-properties "MYSQL_USER")
+                            :password (docker/connection-properties "MYSQL_PASSWORD")}
+          ;when
+          session (target/open-session connection-props)
+          ]
+      ;then
+      (is (= true
+             (target/open? session)))
+      )
+    )
+  (testing "given open session ensure it gets closed"
+    (let [;given
+          connection-props {:host     (docker/host-address @container)
+                            :port     (docker/host-port @container)
+                            :dbname   (docker/connection-properties "MYSQL_DATABASE")
+                            :user     (docker/connection-properties "MYSQL_USER")
+                            :password (docker/connection-properties "MYSQL_PASSWORD")}
+          session (target/open-session connection-props)
+          ;when
+          _ (target/close-session session)
+          ]
+      ;then
+      (is (= false
+             (target/open? session)))
+      )
+    ))
+(deftest schema-validations
+  (testing "given a valid session create schema should succeed"
+    (let [;given
+          connection-props {:host     (docker/host-address @container)
+                            :port     (docker/host-port @container)
+                            :user     "root"
+                            :password "root"}
+          session (target/open-session connection-props)
+          schema-name-fxt (random-string)
+          ;when
+          result (target/create-schema session schema-name-fxt)]
+
+      (is (= (.getName result)
+             schema-name-fxt))
+      )
+    )
+  (testing "attempt to create schema without permission must fail"
+    (let [;given
+          connection-props {:host     (docker/host-address @container)
+                            :port     (docker/host-port @container)
+                            :user     (docker/connection-properties "MYSQL_USER")
+                            :password (docker/connection-properties "MYSQL_PASSWORD")}
+          session (target/open-session connection-props)
+          schema-name-fxt (random-string)]
 
 
-(deftest a-test
-  (testing "FIXME, I don't fail."
-    (is (true? (= 4 (count (str (= 1 1))))))))
+      (is (thrown-with-msg? ExceptionInfo #"Access denied for user" (target/create-schema session schema-name-fxt)))
+      ))
+  (testing "attempt to create duplicated schema must fail"
+    (let [;given
+          connection-props {:host     (docker/host-address @container)
+                            :port     (docker/host-port @container)
+                            :user     "root"
+                            :password "root"}
+          session (target/open-session connection-props)
+          schema-name-fxt (random-string)
+          _ (target/create-schema session schema-name-fxt)]
+
+
+      (is (thrown-with-msg? ExceptionInfo #"^.*database exists*$" (target/create-schema session schema-name-fxt)))
+      ))
+
+  (testing "given a valid schema name to drop should succeed"
+    (with-spy [target/get-schema-names target/drop-schema]
+              (let [;given
+                    connection-props {:host     (docker/host-address @container)
+                                      :port     (docker/host-port @container)
+                                      :user     "root"
+                                      :password "root"}
+                    session (target/open-session connection-props)
+                    schema-name-fxt (random-string)
+                    _ (target/create-schema session schema-name-fxt)
+                    ]
+                ;when
+                (target/drop-schema session schema-name-fxt)
+
+                ;then
+                (is (= 1 (-> target/get-schema-names bond/calls count)))
+                (is (= 1 (-> target/drop-schema bond/calls count)))
+
+                (is (not (contains? (target/get-schema-names session)
+                                    schema-name-fxt)))
+
+                )))
+
+  (testing "given a schema that doesn't exists should not call drop"
+
+    (with-spy [target/get-schema-names target/drop-schema]
+              (let [;given
+                    connection-props {:host     (docker/host-address @container)
+                                      :port     (docker/host-port @container)
+                                      :user     "root"
+                                      :password "root"}
+                    session (target/open-session connection-props)]
+                ;when
+                (target/drop-schema session (random-string))
+
+                ;then
+                (is (= 1 (-> target/get-schema-names bond/calls count)))
+                (is (= 1 (-> target/drop-schema bond/calls count)))
+                )))
+  )
