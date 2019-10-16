778cdd0e87852a1c71b07c7d38524379ee3973d5
Chico Almeida <francisco.almeida@outlook.com>
2019-10-16 11:09:06 -0300

    travis miscellaneous

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/data.json "0.2.6"]
-                 [mysql/mysql-connector-java "8.0.17"]]
+                 [mysql/mysql-connector-java "8.0.18"]]
   :plugins [[lein-cloverage "1.0.13"]
             [lein-shell "0.5.0"]
             [lein-ancient "0.6.15"]
@@ -16,13 +16,13 @@
                                     [test2junit "1.2.7"]
                                     [jonase/eastwood "0.3.5"]
                                     [lein-kibit "0.1.7"]]
-                   :dependencies   [[org.clojure/clojure "1.10.0"]
+                   :dependencies   [[org.clojure/clojure "1.10.1"]
                                     [circleci/bond "0.4.0"]
                                     ; logging
-                                    [ch.qos.logback/logback-classic "1.1.8" :exclusions [org.slf4j/slf4j-api]]
-                                    [org.slf4j/jul-to-slf4j "1.7.22"]
-                                    [org.slf4j/jcl-over-slf4j "1.7.22"]
-                                    [org.slf4j/log4j-over-slf4j "1.7.22"]
+                                    [ch.qos.logback/logback-classic "1.2.3" :exclusions [org.slf4j/slf4j-api]]
+                                    [org.slf4j/jul-to-slf4j "1.7.28"]
+                                    [org.slf4j/jcl-over-slf4j "1.7.28"]
+                                    [org.slf4j/log4j-over-slf4j "1.7.28"]
                                     [org.testcontainers/testcontainers "1.12.2"]]}}
   :deploy-repositories [["releases" :clojars]]
   :aliases {"update-readme-version" ["shell" "sed" "-i" "s/\\\\[mysqlx-clj \"[0-9.]*\"\\\\]/[mysqlx-clj \"${:version}\"]/" "README.md"]}
